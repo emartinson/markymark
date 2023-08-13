5100d5dfc1fd88bd5e5e2f7c0e82b1d88e74c7b6
Eugene Martinson <emartinson@barevtech.com>
2023-08-13 05:45:24 +0300

    add carthage

---
+++ Example/markymark/markymark.h
@@ -0,0 +1,13 @@
+//FILENAME
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for markymark.
+FOUNDATION_EXPORT double markymarkVersionNumber;
+
+//! Project version string for markymark.
+FOUNDATION_EXPORT const unsigned char markymarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <markymark/PublicHeader.h>
+
+
